943a787b5b8832602f97a0b314f26bc10e3955e9
abaddon <abaddon@abaddondeMac-mini.local>
2024-09-03 19:04:14 +0800

    created basic note form

--- app/(content)/home/notes/_components/NoteForm.tsx
+++ app/(content)/home/notes/_components/NoteForm.tsx
@@ -3,11 +3,21 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Dispatch, SetStateAction } from "react";
 import { DetailedNote } from "./types";
-import { Form } from "@/components/ui/form";
+import { Form, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import toast from 'react-hot-toast';
+import { Input } from "@/components/ui/input";
+import { Combobox } from "@/components/ui/combobox";
+import { NoteStatus } from "@prisma/client";
+import { Switch } from "@/components/ui/switch";
+import axios from "axios";
+
+
+function extractLevelOneKeys<T extends object>(obj: T): Array<keyof T> {
+    return Object.keys(obj) as Array<keyof T>;
+}
 
 const formSchema = z.object({
     id: z.string().optional(),
@@ -30,9 +40,10 @@ const NoteForm = ({existingNote, mode, setMode}: {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            status: 'Draft',
-            stared: false,
-            hidden: false
+            id: existingNote?.id,
+            status: existingNote?.status || 'Draft',
+            stared: existingNote?.stared || false,
+            hidden: existingNote?.hidden || false
         }
     });
 
@@ -40,7 +51,8 @@ const NoteForm = ({existingNote, mode, setMode}: {
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-            
+            const noteResponse = await axios.post('/api/note', values);
+            console.log(noteResponse.data)
         } catch (error) {
             console.log(error);
             toast.error('Something Went Wrong')
@@ -49,10 +61,97 @@ const NoteForm = ({existingNote, mode, setMode}: {
 
     return (
         <Form {...form}>
-            <form>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-2">
+                <FormField
+                    name='title'
+                    control={form.control}
+                    render={({field}) => {
+                        return (
+                            <FormItem>
+                                <FormLabel>
+                                    Title
+                                </FormLabel>
+                                <Input
+                                    {...field}
+                                >
+                                </Input>
+                            </FormItem>
+                        )
+                    }}
+                >
+                </FormField>
+                
+                
+                <FormField
+                    name='status'
+                    control={form.control}
+                    render={({field}) => {
+                        return (
+                            <FormItem>
+                                <FormLabel>
+                                    Title
+                                </FormLabel>
+                                <Combobox
+                                    options={(extractLevelOneKeys(NoteStatus) as [string, ...string[]]).map(status => ({
+                                        value: status,
+                                        label: status.toLocaleUpperCase(),
+                                    }))}
+                                    {...field}
+                                />
+                            </FormItem>
+                        )
+                    }}
+                >
+                </FormField>
+
+                <FormField
+                    name='stared'
+                    control={form.control}
+                    render={({field}) => {
+                        return (
+                            <FormItem className="flex flex-row justify-between items-center">
+                                <FormLabel>
+                                    Star
+                                </FormLabel>
+                                <Switch
+                                    checked={form.watch('stared')}
+                                    onCheckedChange={(event) => {
+                                        console.log(event);
+                                        form.setValue('stared', event )
+                                    }}
+                                >
+                                </Switch>
+                            </FormItem>
+                        )
+                    }}
+                >
+                </FormField>
+
+                <FormField
+                    name='hidden'
+                    control={form.control}
+                    render={({field}) => {
+                        return (
+                            <FormItem className="flex flex-row justify-between items-center">
+                                <FormLabel>
+                                    Hidden
+                                </FormLabel>
+                                <Switch
+                                    checked={form.watch('hidden')}
+                                    onCheckedChange={(event) => {
+                                        console.log(event);
+                                        form.setValue('hidden', event )
+                                    }}
+                                >
+                                </Switch>
+                            </FormItem>
+                        )
+                    }}
+                >
+                </FormField>
 
                 <div className="flex flex-row gap-2 justify-end">
-                    <Button type={'submit'}>Submit</Button>
+                    <Button type="submit">Submit</Button>
                     <Button 
                     className="bg-white border-2 border-gray-900 text-red-600 hover:bg-slate-100"
                     type={'button'}
--- app/api/note/route.ts
+++ app/api/note/route.ts
@@ -1,7 +1,22 @@
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+    try {
+        const data = await req.json();
+        console.log(data);
 
+        return new NextResponse(JSON.stringify({
+            message: 'OK'
+        }), {
+            status: 200
+        });
+    } catch (error) {
+        return new NextResponse(JSON.stringify({
+            message: 'Failed'
+        }), {
+            status: 500
+        })
+    }
 }
 
 export async function PATCH(req: Request) {
