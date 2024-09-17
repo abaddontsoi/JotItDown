5123b63045262736716f38bcaa67aab1fc934626
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2024-09-17 17:03:07 +0800

    Progress on Create Task

--- components/self-defined/TaskInfoDialog.tsx
+++ components/self-defined/TaskInfoDialog.tsx
@@ -1,26 +1,41 @@
 import { Dispatch, SetStateAction } from "react";
-import { Dialog, DialogContent } from "../ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
 import { TaskInfo } from "@prisma/client";
+import TaskInfoForm from "./TaskInfoForm";
 
 interface TaskInfoDialogProp {
     existingTaskInfo?: TaskInfo,
-    mode: 'Edit' | 'Create' | 'Close', 
+    mode: 'Edit' | 'Create' | 'Close',
     setTargetTaskInfo?: Dispatch<SetStateAction<TaskInfo>>,
     setMode: Dispatch<SetStateAction<"Edit" | "Create" | "Close">>,
 }
 
 const TaskInfoDialog = ({
     mode,
-    setMode
+    existingTaskInfo,
+    setMode,
+    setTargetTaskInfo
 }: TaskInfoDialogProp) => {
     return (
         <Dialog open={mode != 'Close'}
-        onOpenChange={() => {
-            setMode('Close');
-        }}
+            onOpenChange={() => {
+                setMode('Close');
+            }}
         >
             <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        {mode} Task
+                    </DialogTitle>
+                </DialogHeader>
 
+
+                <TaskInfoForm
+                    mode={mode}
+                    existingTaskInfo={existingTaskInfo}
+                    setMode={setMode}
+                    setTargetTaskInfo={setTargetTaskInfo}
+                />
             </DialogContent>
         </Dialog>
     )
