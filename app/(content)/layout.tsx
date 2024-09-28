4da63b2cd69a3f435a9e6330ed22bb4922a43517
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2024-09-28 14:27:02 +0800

    Added analytics

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import NavSideBar from "../../components/self-defined/NavSideBar";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +18,12 @@ export default function RootLayout({
     return (
         <main className="flex flex-row bg-gray-200">
             {/* this is the navigation side bar */}
-                <NavSideBar />
+            <NavSideBar />
             {/* <div className="bg-gray-200 w-fit sticky h-screen">
             </div> */}
             <div className="bg-slate-100 w-screen p-[20px]">
                 {children}
+                <Analytics />
             </div>
         </main>
     );
