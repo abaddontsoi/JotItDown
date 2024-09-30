4d8ecf7b82df6c387dd6344d7a9801e0459a1322
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2024-09-30 18:50:35 +0800

    Updated Note edit for other sections in note page

--- components/self-defined/StaredNotesContextCard.tsx
+++ components/self-defined/StaredNotesContextCard.tsx
@@ -8,6 +8,7 @@ import { Suspense, useState } from "react";
 import { Accordion, AccordionContent, AccordionItem } from "@/components/ui/accordion";
 import { AccordionHeader, AccordionTrigger } from "@radix-ui/react-accordion";
 import { ArrowDown, ArrowUp } from "lucide-react";
+import NoteDialog from "./NoteDialog";
 
 const FallBack = () => {
     return (
@@ -26,8 +27,18 @@ const StaredNotesContextCard = (
 ) => {
     const router = useRouter();
     const [accordionOpen, setOpen] = useState<boolean>(false);
+
+    const [mode, setMode] = useState<'Edit' | 'Create' | 'Close'>('Close');
+    const [targetNote, setTargetNote] = useState<DetailedNote | undefined>();
+
     return (
         <>
+            <NoteDialog
+                mode={mode}
+                setMode={setMode}
+                existingNote={targetNote}
+            />
+
             <Card>
                 <Accordion type="multiple">
                     <AccordionItem value="list">
@@ -53,7 +64,11 @@ const StaredNotesContextCard = (
                                 {
                                     notes?.filter(n => n.stared && !n.hidden).map(n => (
                                         <Suspense key={n.id} fallback={<FallBack />}>
-                                            <NoteDisplay key={n.id} note={n}></NoteDisplay>
+                                            <NoteDisplay 
+                                            key={n.id} note={n}
+                                            setMode={setMode}
+                                            setNote={setTargetNote}
+                                            ></NoteDisplay>
                                         </Suspense>
                                     ))
                                 }
