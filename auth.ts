3800fc6d0d3236e07cf91208b559142a434ec10a
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2024-10-11 10:23:44 +0800

    Add github OAuth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import Okta from "next-auth/providers/okta";
+import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Okta],
+    providers: [Okta, Google, GitHub],
 })
