ffa97b1dd97d0241c594e1c4b3ebe95f8b4b5854
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2024-10-11 09:48:26 +0800

    Add buttons for other login providers

--- components/self-defined/LoginCard.tsx
+++ components/self-defined/LoginCard.tsx
@@ -7,19 +7,53 @@ interface LoginCardProp {
 
 }
 
+
 const LoginCard = (
-    {}: LoginCardProp
+    { }: LoginCardProp
 ) => {
+    const oktaLogin = async () => {
+        "use server";
+        await signIn("okta", {
+            redirectTo: "/home"
+        });
+    }
+
+    const googleLogin = async () => {
+        "use server";
+        await signIn("google", {
+            redirectTo: "/home"
+        });
+    }
+    const githubLogin = async () => {
+        "use server";
+        await signIn("github", {
+            redirectTo: "/home"
+        });
+    }
     return (
         <>
-        <form action={async () => {
-            "use server";
-            await signIn("okta");
-        }}>
-            <Button type="submit">
-                Sign In With Okta
-            </Button>
-        </form>
+            {/* sign in with okta */}
+            <form action={oktaLogin}>
+                <Button type="submit">
+                    Sign In With Okta
+                </Button>
+            </form>
+
+
+            {/* sign in with google */}
+            <form action={googleLogin}>
+                <Button type="submit">
+                    Sign In With Google
+                </Button>
+            </form>
+
+            {/* sign in with github */}
+            <form action={githubLogin}>
+                <Button type="submit">
+                    Sign In With GitHub
+                </Button>
+            </form>
+
         </>
     );
 }
