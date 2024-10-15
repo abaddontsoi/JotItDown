46259a464acc2860832784a880b8f4283dcbe608
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2024-10-15 14:05:30 +0800

    Update form and API to allow edit group info

--- components/self-defined/GroupCardsContainer.tsx
+++ components/self-defined/GroupCardsContainer.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import { useState } from "react";
 import GroupCard from "./GroupCard";
-import { DetailedGroup } from "./types";
+import { DetailedGroup, DialogModes } from "./types";
+import GroupDialog from "./GroupDialog";
 
 interface GroupCardsContainerProp {
     groups: DetailedGroup[]
@@ -12,16 +14,30 @@ export default function GroupCardsContainer(
         groups
     }: GroupCardsContainerProp
 ) {
-    return (
-        <div>
-            {
-                groups.map(group => {
-                    return (
-                        <GroupCard group={group} />
-                    )
-                })
-            }
-        </div>
+    const [mode, setMode] = useState<DialogModes>('Close');
+    const [group, setGroup] = useState<DetailedGroup>();
 
+    return (
+        <>
+            <GroupDialog
+                mode={mode}
+                existingGroup={group}
+                setMode={setMode}
+                setGroup={setGroup}
+            />
+            <div className="flex items-center gap-3 flex-wrap">
+                {
+                    groups.map(group => {
+                        return (
+                            <GroupCard
+                                group={group}
+                                setMode={setMode}
+                                setGroup={setGroup}
+                            />
+                        )
+                    })
+                }
+            </div>
+        </>
     )
 }
