9476d4606aa4edd2794d56eca76b9a1abc9d9861
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2024-10-16 08:00:44 +0800

    Update UI and Schema

--- components/self-defined/GroupNotesContainer.tsx
+++ components/self-defined/GroupNotesContainer.tsx
@@ -4,11 +4,14 @@ interface GroupNotesContainerProp {
 
 export default function GroupNotesContainer(
     {
-        
+
     }: GroupNotesContainerProp
 ) {
     return (
         <>
+            <div>
+                <h2 className="text-2xl">Notes</h2>
+            </div>
         </>
     );
 }
