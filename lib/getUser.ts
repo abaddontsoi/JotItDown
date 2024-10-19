bbf6ca093e9702a7d0837277eae8bda449633ebc
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2024-10-19 12:16:57 +0800

    Revert "Merge branch 'main' into inbox-page"
    This reverts commit 9020cbd334e8124d3c49d04d0ca17b27443cfcb1, reversing
    changes made to 8c4c7b5eddbe6229eaf3b5ba4d6063f3399bad23.

--- lib/getUser.ts
+++ lib/getUser.ts
@@ -11,7 +11,7 @@ export const getUser = async () => {
 
     const userInDB = await db.user.findFirst({
         where: {
-            email: session?.user?.email || null
+            email: session?.user?.email
         }
     })
 
