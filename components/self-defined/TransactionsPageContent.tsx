26b57d24e154fa28682bd1f792110c650bcf3b4d
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2024-11-06 00:53:41 +0800

    Add date filter for transactions

--- components/self-defined/TransactionsPageContent.tsx
+++ components/self-defined/TransactionsPageContent.tsx
@@ -4,6 +4,9 @@ import { CashAccount } from "@prisma/client";
 import { DetailedTransaction, DialogModes } from "./types";
 import TransactionsTableCard from "./TransactionsTableCard";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
 
 interface TransactionsPageContentProp {
     transactions: DetailedTransaction[];
@@ -20,11 +23,42 @@ export default function TransactionsPageContent(
         setTransaction,
     }: TransactionsPageContentProp
 ) {
+    const [startDate, setStartDate] = useState<Date>();
+    const [endDate, setEndDate] = useState<Date>(new Date());
+
     return (
         <>
             <Card>
-                <CardHeader>
-                    <CardTitle>All transactions</CardTitle>
+                <CardHeader className="flex flex-row items-center justify-between">
+                    <CardTitle className="w-fit">All transactions</CardTitle>
+
+                    {/* Place a date filter component here */}
+                    <div className="flex gap-2">
+                        <div>
+                            <Label>Start Date</Label>
+                            {/* start date */}
+                            <Input
+                                className="w-fit"
+                                type="date"
+                                onChange={(event) => {
+                                    setStartDate(new Date(event.target.value));
+                                }}
+                            />
+                        </div>
+
+                        <div>
+                            <Label>End Date</Label>
+                            {/* end date */}
+                            <Input
+                                className="w-fit"
+                                type="date"
+                                onChange={(event) => {
+                                    setEndDate(new Date(event.target.value));
+                                }}
+                                defaultValue={(new Date()).toISOString()}
+                            />
+                        </div>
+                    </div>
                 </CardHeader>
                 <CardContent>
                     <TransactionsTableCard
@@ -32,7 +66,10 @@ export default function TransactionsPageContent(
                         accounts={accounts}
                         setMode={setMode}
                         setTransaction={setTransaction}
+
                         // Date filters
+                        startDate={startDate}
+                        endDate={endDate}
                     />
                 </CardContent>
             </Card>
