ebd9ab8e46796b4acfa751fd34e4fc45d766657c
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2024-12-02 12:24:39 +0800

    Revert "Update api"
    This reverts commit 28b975c092498f2f7e517b6ec832e37d02eee977.

--- app/(content)/home/budget/page.tsx
+++ app/(content)/home/budget/page.tsx
@@ -41,7 +41,7 @@ const BudgetPage = async () => {
     const allAccountsRecords: PromiseDetailedAccountRecords = db.itemAccount.findMany({
         where: {
             belongToId: user.id,
-            // isDisabled: false,
+            isDisabled: false,
         },
         include: {
             CashFlow: {
